433b788ec7bf91e4220f008b1b2f6bcef1ffbde4
Luciano Oliveira <luciano101@msn.com>
2017-10-21 11:46:00 -0700

    console.log doctor's first name, last name, address and phone

--- output.js
+++ output.js
@@ -83,6 +83,7 @@ $('#update').click(function(e) {
 var doctorName;
 function getDoctorName(){ 
   doctorName = $('.doctor-name').val();
+  specialty = $('.doctor-specialty').val();
 
   $.ajax({
     url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`,
@@ -91,7 +92,7 @@ function getDoctorName(){
       success: function(response) {
         console.log(response.data);
         for(var i = 0; i < response.data.length; i++){
-          console.log('name ' + response.data[i].specialties.uid);
+          console.log(`name ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} ${response.data[i].practices[0].phones[0].number} ${response.data[i].practices[0].visit_address.city}`);
         }
 
       }
